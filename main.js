3a5bff11cec07ac647f58b93503741084a739db0
cbrobak <cbrobak79@gmail.com>
2024-11-13 05:50:08 -0500

    Updated styles and functionality.
    Added some styling in css and used js to set the affirmation container to display: none when there is no text, removed comments, and deleted some changed code.

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
+const affirmationContainer = document.getElementById("affirmationContainer");
+const affirmation = document.getElementById("affirmation");
+
+affirmation.innerText = " "
+  ? (affirmationContainer.style.display = "none")
+  : (affirmationContainer.style.display = "inline-block");
+// ? (affirmationContainer.style.display = "none")
+// : (affirmationContainer.style.display = "block");
+
 function getAffirmations() {
   const url =
     "https://api.allorigins.win/get?url=" +
     encodeURIComponent("https://www.affirmations.dev") +
     "&_=" +
-    new Date().getTime(); // Added a timestamp to URL to fetch a new affirmation with each call
+    new Date().getTime();
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const affirmation = JSON.parse(data.contents).affirmation;
-      document.getElementById("affirmation").innerText = affirmation;
+      const affirmationText = JSON.parse(data.contents).affirmation;
+      affirmation.textContent = affirmationText; // Update the affirmation text
     })
     .catch((error) => {
       console.error("Error fetching affirmation:", error);
-      document.getElementById("affirmation").innerText =
-        "Oops! Something went wrong.";
     });
 }
